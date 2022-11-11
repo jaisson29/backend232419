3b5a0d9bb321c54ea6cdd9d67e39326ec3796d08
Jaisson Valbuena <jaissonvalbuena29@outlook.com>
2022-11-10 20:52:09 -0500

    add id into product model

--- src/app.js
+++ src/app.js
@@ -11,7 +11,20 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/slug/:slug', (req, res) => {
-    const product = datafull.products.find( (x) => x.slug == req.params.slug);
+    const product = datafull.products.find((x) => x.slug === req.params.slug);
+    console.log(product);
+    if(product) {
+        res.send(product);
+    }
+    else
+    {
+        res.status(404).send({message: 'Producto no encontrado'});
+    }
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = datafull.products.find((x) => x._id === req.params.id);
+    console.log(product);
     if(product) {
         res.send(product);
     }
